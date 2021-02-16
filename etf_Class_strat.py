57bcf833278637591111318010267bf2bb597112
Yushi Wei <58374480+globalize9@users.noreply.github.com>
2021-02-16 08:51:20 -0800

    testing_BB

--- etf_Class_strat.py
+++ etf_Class_strat.py
@@ -10,6 +10,7 @@ import numpy as np
 import investpy
 import yfinance as yf
 from datetime import *
+import matplotlib.pyplot as plt
 
 class ETF:
     def __init__(self, ticker, country, start_date, end_date):
@@ -72,7 +73,7 @@ data = yf.download(  # or pdr.get_data_yahoo(...
         # use "period" instead of start/end
         # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
         # (optional, default is '1mo')
-        period = "5y",
+        period = "1y",
 
         # fetch data by interval (including intraday if period < 60 days)
         # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
@@ -106,14 +107,14 @@ stock_close = data.xs('Close', axis = 1, level = 1, drop_level = True)
 def BollingerBand(stocks, window=21, no_of_std=2):
         
         # initiate a dataframe
-        data = pd.DataFrame(stocks.values, columns = ['Close'])
+        data = pd.DataFrame(stocks.values, index = stocks.index, columns = ['Close'])
         
         rolling_mean = data['Close'].rolling(window).mean()
         rolling_std = data['Close'].rolling(window).std()
         
         data['Rolling_Mean'] = rolling_mean
         data['Bollinger_Upper'] = data['Rolling_Mean'] + rolling_std * no_of_std
-        data['Bolling_Lower'] = data['Rolling_Mean'] - rolling_std * no_of_std
+        data['Bollinger_Lower'] = data['Rolling_Mean'] - rolling_std * no_of_std
         
         data.plot(color=['black','red','green','green'], title = 'BB')
         return data
@@ -122,7 +123,42 @@ tt = BollingerBand(stock_close['MSFT'])
 
 
 # testing trading strat based on BB
+df = tt.copy()
 
+#Create an "empty" column as placeholder for our /position signals
+df['Position'] = None
+
+#Fill our newly created position column - set to sell (-1) when the price hits the upper band, and set to buy (1) when it hits the lower band
+for row in range(len(df)):
+    
+    if (df['Close'].iloc[row] > df['Bollinger_Upper'].iloc[row]) and (df['Close'].iloc[row-1] < df['Bollinger_Upper'].iloc[row-1]):
+        df['Position'].iloc[row] = -1
+        
+    if (df['Close'].iloc[row] < df['Bollinger_Lower'].iloc[row]) and (df['Close'].iloc[row-1] > df['Bollinger_Lower'].iloc[row-1]):
+        df['Position'].iloc[row] = 1  
+    
+    # think about how to close a long position
+    if df['Position'].iloc[row] == 1:
+         if (df['Close'].iloc[row] < df['Rolling_Mean'].iloc[row]) and (df['Close'].iloc[row-1] > df['Rolling_Mean'].iloc[row-1]):
+             df.loc[df.index[row],'Position'] = 0
+    
+    # how to close a short position
+    if df['Position'].iloc[row] == -1:
+         if (df['Close'].iloc[row] > df['Rolling_Mean'].iloc[row]) and (df['Close'].iloc[row-1] < df['Rolling_Mean'].iloc[row-1]):
+             df.loc[df.index[row],'Position'] = 0
+             
+
+#Forward fill our position column to replace the "None" values with the correct long/short positions to represent the "holding" of our position
+#forward through time
+df['Position'].fillna(method='ffill',inplace=True)
+
+#Calculate the daily market return and multiply that by the position to determine strategy returns
+df['Market Return'] = np.log(df['Close'] / df['Close'].shift(1))
+df['Strategy Return'] = df['Market Return'] * df['Position'].shift(1)
+
+#Plot the strategy returns
+plt.figure()
+df['Strategy Return'].cumsum().plot(title = 'Cumulative Return')
 
 
 
